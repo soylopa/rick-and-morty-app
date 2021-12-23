0290355717d9a6a245128dcee9e6bcb9118d75c7
soylopa <paolaenbcn@hotmail.com>
2021-12-23 16:35:11 +0100

    nav-bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = ({brand}) => {
     return (
-        <div className="navbar navbar-dark bg-dark">
+        <div className="navbar navbar-light bg-light">
             <div className="container">
             <h1 className="navbar-brand"> {brand}</h1>
             </div>
