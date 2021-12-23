9d303a5a0bb837d32e7dfa7f337f3844f66f6113
soylopa <paolaenbcn@hotmail.com>
2021-12-23 16:37:42 +0100

    links-nav-bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,10 @@ const Navbar = ({brand}) => {
         <div className="navbar navbar-light bg-light">
             <div className="container">
             <h1 className="navbar-brand"> {brand}</h1>
+            <a href=""> Locations </a>
+            <a href=""> Episodes </a>
+            <a href="">  Characters </a>
+            <a href=""> Chart-icon</a>
             </div>
         </div>
     )
